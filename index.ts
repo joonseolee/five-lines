4150ef22c7be718c23cc23118ff152a30f5ec446
joonseolee <niceweather94@gmail.com>
2023-04-15 11:42:54 +0900

    refactor: strategy 패턴 구체화

--- index.ts
+++ index.ts
@@ -46,12 +46,17 @@ class FallStrategy {
     return this.falling;
   }
   update(tile: Tile, x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      map[y][x].drop();
+    this.falling = map[y + 1][x].isAir() 
+    ? new Falling()
+    : new Resting();
+
+    this.drop(tile, x, y);
+  }
+
+  drop(tile: Tile, x: number, y: number) {
+    if (this.falling.isFalling()) {
       map[y + 1][x] = tile;
       map[y][x] = new Air();
-    } else {
-      map[y][x].rest();
     }
   }
 }
